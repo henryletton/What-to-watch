de6ab6ed9ffc09611bf66b6199fb1849a26fafb3
HennersMcGee <henry_letton@hotmail.com>
2021-03-17 10:30:36 +0000

    Update film scraping to only add and remove films instead of fetching all film data everytime. Also added catch for film page issues.

--- src/data_sourcing_v2.py
+++ src/data_sourcing_v2.py
@@ -7,13 +7,13 @@
 '''
 
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 from src.db_fns import create_engine2, df_to_sql_db, sql_db_to_df
+import numpy as np
 
+#%% Loop through site to get full list of film urls
 # Define list of all catelogue pages with films on
 catelogue_urls_short = [f'/catalogue/a2z/all/{catelogue_page}' for catelogue_page in list('abcdefghijklmnopqrstuvwxyz')]
 catelogue_urls_short.insert(0, '/catalogue/a2z/all')
@@ -27,7 +27,6 @@ film_urls_short = []
 headers = {'User-Agent':
            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
            ' Chrome/86.0.4240.193 Safari/537.36'}
-
 
 # Loop through catelogue_urls
 while catelogue_urls_short:
@@ -62,10 +61,32 @@ while catelogue_urls_short:
     catelogue_urls_short.pop(0)
     catelogue_urls_short_compl.append(catelogue_url_short)
 
+#%% Use film urls and previously stored films to get new and removed Netflix film lists
 # Dedup film links
 film_urls_short = list(set(film_urls_short))
 film_urls_short.sort()
+film_ids = [film_url_short.replace('/info/','') for film_url_short in film_urls_short]
 
+# Get currently stored film ids
+engine = create_engine2()
+W2W_Films = sql_db_to_df(engine, 'W2W_Films')
+
+# Get list of newly added films, and convert to urls
+new_film_ids = list(np.setdiff1d(film_ids,list(W2W_Films['film_key'])))
+new_film_urls_short = [f'/info/{film_ids}' for film_ids in new_film_ids]
+
+# Get list of removed films and convert to int
+rmv_film_ids = list(np.setdiff1d(list(W2W_Films['film_key']),film_ids))
+rmv_film_ids_str = [str(film_id) for film_id in rmv_film_ids]
+
+#%% Remove films no longer on Netflix
+if len(rmv_film_ids) > 0:
+    drop_rmv_films_sql = f'DELETE FROM W2W_Films WHERE film_key in ({",".join(rmv_film_ids_str)});'
+    
+    with engine.begin() as cnx:
+        cnx.execute(drop_rmv_films_sql)
+
+#%% Get new to Netflix film information
 # Store past films in this list
 film_urls_done = []
 
@@ -74,69 +95,74 @@ film_dicts = []
 
 # Loop through films 
 count = 1
-while film_urls_short:
+while new_film_urls_short:
 
-    film_url_short = film_urls_short[0]
+    film_url_short = new_film_urls_short[0]
     
     film_url = f'https://uk.newonnetflix.info{film_url_short}'
     response = requests.get(film_url, headers=headers)
     print(f'{count} {film_url} - {response.status_code}') # Count of films instead
     
-    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Dictionary to store film info
+        film_dict = {}
+        
+        film_dict['film_key'] = int(film_url_short.split('/')[2])
+        
+        film_dict['title'] = soup.select('h1 a')[0].text
+        
+        # Any information stored in p parts, is extracted based on text matching logic (as the webpage can change between films)
+        all_p_text = [p.text for p in soup.find_all('p')]
     
-    # Dictionary to store film info
-    film_dict = {}
+        desc_next = False
+        for p_text in all_p_text:
+            if 'Year: ' in p_text:
+                film_dict['year'] = int(p_text[6:10])
+            elif 'Duration: ' in p_text:
+                film_dict['duration'] = p_text[10:]
+            elif desc_next:
+                film_dict['description'] = p_text
+                desc_next = False
+            elif 'Description:' in p_text:
+                desc_next = True
+        
+        film_dict['genres'] = soup.select('h5')[0].text
+        
+        # Rating information changes betwen films too, and can be in a different form
+        ratings = list(set([rating['title'] for rating in soup.select('p > .starrating')]))
+        
+        for rating in ratings:
+            rating2 = rating.split(' rating ')
+            if len(rating2) > 1:
+                score_type = rating2[0].replace(' ', '_').lower()
+                if '/' in rating2[1]:
+                    score2 = rating2[1].split('/')
+                    score = float(score2[0]) / float(score2[1])
+                if '%' in rating2[1]:
+                    score2 = rating2[1].split('%')
+                    score = float(score2[0]) / 100
+                
+                film_dict[f'{score_type}_rating'] = score
+        
+        film_dicts.append(film_dict)
+    except:
+        print('Issue with this film page')
     
-    film_dict['film_key'] = int(film_url_short.split('/')[2])
-    
-    film_dict['title'] = soup.select('h1 a')[0].text
-    
-    # Any information stored in p parts, is extracted based on text matching logic (as the webpage can change between films)
-    all_p_text = [p.text for p in soup.find_all('p')]
-
-    desc_next = False
-    for p_text in all_p_text:
-        if 'Year: ' in p_text:
-            film_dict['year'] = int(p_text[6:10])
-        elif 'Duration: ' in p_text:
-            film_dict['duration'] = p_text[10:]
-        elif desc_next:
-            film_dict['description'] = p_text
-            desc_next = False
-        elif 'Description:' in p_text:
-            desc_next = True
-    
-    film_dict['genres'] = soup.select('h5')[0].text
-    
-    # Rating information changes betwen films too, and can be in a different form
-    ratings = list(set([rating['title'] for rating in soup.select('p > .starrating')]))
-    
-    for rating in ratings:
-        rating2 = rating.split(' rating ')
-        if len(rating2) > 1:
-            score_type = rating2[0].replace(' ', '_').lower()
-            if '/' in rating2[1]:
-                score2 = rating2[1].split('/')
-                score = float(score2[0]) / float(score2[1])
-            if '%' in rating2[1]:
-                score2 = rating2[1].split('%')
-                score = float(score2[0]) / 100
-            
-            film_dict[f'{score_type}_rating'] = score
-    
-    film_dicts.append(film_dict)
     
     count += 1
-    film_urls_done.append(film_url_short)
-    film_urls_short.pop(0)
+    film_urls_done.append(new_film_urls_short)
+    new_film_urls_short.pop(0)
 
 # Convert list of dictionaries into a dataframe
 film_df = pd.DataFrame(film_dicts)
 film_df['platform'] = 'Netflix'
 
 
-#%% Connect to My SQL database and insert film data
-engine = create_engine2()
-
-W2W_Films = df_to_sql_db(engine = engine, df_write = film_df, 
-                         table_name = 'W2W_Films2', replace = True)
+#%% Insert new film data
+# Connect to database
+engine.connect()
+if len(film_dicts) > 0:
+    W2W_Films = df_to_sql_db(engine = engine, df_write = film_df, 
+                             table_name = 'W2W_Films', replace = True)
