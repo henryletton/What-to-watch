947cc9d9310ebc702fa46825ae132e41c285b77e
Max <maxwellleonardi@googlemail.com>
2021-05-07 16:52:29 +0100

    Fix

--- tests/db_fns/test_comedy_replace.py
+++ tests/db_fns/test_comedy_replace.py
@@ -10,6 +10,6 @@ def test_for_comedy_replace():
 # Check "comedies" is replaced correctly"
 def test_for_comedies_replace():
     engine = create_engine2()
-    search_df = search_film(engine, "The IT Crowd", "comedy")
+    search_df = search_film(engine, "The IT Crowd", "comedies")
     assert "The IT Crowd" in search_df["title"].values
 
